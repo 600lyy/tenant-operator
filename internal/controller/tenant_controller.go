3c403d091a00396c2fd13ca7b57c3b9016dcdaf8
Andy Lu <luyul@google.com>
2024-01-11 17:37:15 +0100

    first bulk commit.

---
+++ internal/controller/tenant_controller.go
@@ -0,0 +1,151 @@
+/*
+Copyright 2024.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controller
+
+import (
+	"context"
+
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+
+	multitenancyv1 "github.com/600lyy/tenant-operator/api/v1"
+)
+
+var (
+	tenantOperatorAnnotation = "tenant-operator"
+)
+
+// TenantReconciler reconciles a Tenant object
+type TenantReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+}
+
+//+kubebuilder:rbac:groups=multitenancy.600lyy.io,resources=tenants,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=multitenancy.600lyy.io,resources=tenants/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=multitenancy.600lyy.io,resources=tenants/finalizers,verbs=update
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the Tenant object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.16.3/pkg/reconcile
+
+// +kubebuilder:rbac:groups=multitenancy.600lyy.io,resources=*,verbs=*
+// +kubebuilder:rbac:groups="",resources=namespaces,verbs=*
+// +kubebuilder:rbac:groups=rbac.authorization.k8s.io,resources=*,verbs=*
+func (r *TenantReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	log := log.FromContext(ctx)
+
+	// TODO(user): your logic here
+	tenant := multitenancyv1.Tenant{}
+
+	log.Info("Reconciling tenant")
+
+	if err := r.Get(ctx, req.NamespacedName, &tenant); err != nil {
+		log.Error(err, "Object not found")
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	// Loop through each namespace defined in the Tenant Spec
+	// Ensure the namespace exists, and if not, create it
+	// Then ensure RoleBindings for each namespace
+	for _, ns := range tenant.Spec.Namespaces {
+		log.Info("Ensuring namespace", "namespace", ns)
+		if err := r.EnsureNamespace(ctx, &tenant, ns); err != nil {
+			log.Error(err, "unable to ensure namespace", "namespace", ns)
+			return ctrl.Result{}, err
+		}
+	}
+
+	tenant.Status.NamespaceCount = int32(len(tenant.Spec.Namespaces))
+	tenant.Status.AdminEmail = tenant.Spec.AdminEmail
+	if err := r.Status().Update(ctx, &tenant); err != nil {
+		log.Error(err, "unable to update Tenant status")
+		return ctrl.Result{}, err
+	}
+
+	return ctrl.Result{}, nil
+}
+
+func (r *TenantReconciler) EnsureNamespace(ctx context.Context, tenant *multitenancyv1.Tenant, namespaceName string) error {
+	log := log.FromContext(ctx)
+
+	namespace := corev1.Namespace{}
+
+	//Attempt to fetch the namespace, otherwise creat a new one if it doesn't exist
+	err := r.Get(ctx, client.ObjectKey{Name: namespaceName}, &namespace)
+	if err != nil {
+		if apierrors.IsNotFound(err) {
+			log.Info("Creating namespace", "ns", namespaceName)
+			namespace = corev1.Namespace{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: namespaceName,
+					Annotations: map[string]string{
+						"adminEmail": tenant.Spec.AdminEmail,
+						"managed-by": tenantOperatorAnnotation,
+					},
+				},
+			}
+			if err = r.Create(ctx, &namespace); err != nil {
+				return err
+			}
+		} else {
+			return err
+		}
+	} else {
+		log.Info("Namespace already exists", "namespace", namespaceName)
+
+		if namespace.Annotations == nil {
+			namespace.Annotations = map[string]string{}
+		}
+
+		requiredAnnotions := map[string]string{
+			"adminEmail": tenant.Spec.AdminEmail,
+			"managed-by": tenantOperatorAnnotation,
+		}
+
+		for annotaionKey, requiredValue := range requiredAnnotions {
+			existingAnnotation, ok := namespace.Annotations[annotaionKey]
+			if !ok || existingAnnotation != requiredValue {
+				log.Info("Updating namespace annotation", "namespace", namespace.Name, "annotation", annotaionKey)
+				namespace.Annotations[annotaionKey] = requiredValue
+				if err := r.Update(ctx, &namespace); err != nil {
+					return err
+				}
+			}
+		}
+	}
+
+	return nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *TenantReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&multitenancyv1.Tenant{}).
+		Complete(r)
+}
