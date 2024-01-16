d085e77af338a03d3f63dd90dc3e39d8199cd591
Andy Lu <luyul@google.com>
2024-01-16 14:41:13 +0100

    add permissions to the clusterrole to list namespaces.

--- internal/controller/tenant_controller.go
+++ internal/controller/tenant_controller.go
@@ -45,20 +45,10 @@ type TenantReconciler struct {
 //+kubebuilder:rbac:groups=multitenancy.600lyy.io,resources=tenants,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=multitenancy.600lyy.io,resources=tenants/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=multitenancy.600lyy.io,resources=tenants/finalizers,verbs=update
+//+kubebuilder:rbac:groups=multitenancy.600lyy.io,resources=*,verbs=*
+//+kubebuilder:rbac:groups="",resources=namespaces,verbs=*
+//+kubebuilder:rbac:groups=rbac.authorization.k8s.io,resources=*,verbs=*
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the Tenant object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.16.3/pkg/reconcile
-
-// +kubebuilder:rbac:groups=multitenancy.600lyy.io,resources=*,verbs=*
-// +kubebuilder:rbac:groups="",resources=namespaces,verbs=*
-// +kubebuilder:rbac:groups=rbac.authorization.k8s.io,resources=*,verbs=*
 func (r *TenantReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	log := log.FromContext(ctx)
 
